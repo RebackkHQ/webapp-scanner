a778b5a97140fb67cf503d72dac59adc571316a3
Sarwagya <hello@esportzvio.com>
2024-11-07 22:15:03 +0100

    fix: fixed compatibility with npx
    1. added `#!/usr/bin/env node` in src/index.ts for npx compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
+#!/usr/bin/env node
+
 import { Command } from "commander";
-// @ts-ignore
+// @ts-ignore: For TypeScript compatibility when importing JSON files
 import packageData from "../package.json";
 
+// Create a new Command object
 const program = new Command();
 
-program.version(packageData.version);
-program.name(packageData.name);
-program.description(packageData.description);
+// Set version, name, and description from the package.json
+program
+  .version(packageData.version)
+  .name(packageData.name)
+  .description(packageData.description);
 
-program.helpCommand(true);
+// Add a help command explicitly if needed
+program.helpOption("-h, --help", "Display help for command");
 
-program.parse();
+// Parse command-line arguments
+program.parse(process.argv);
+
+const options = program.opts();
+
+// If no arguments are provided, display help
+if (Object.keys(options).length === 0) {
+  program.help();
+}
